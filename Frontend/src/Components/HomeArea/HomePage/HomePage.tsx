34cb4e69d9b1d02ad69a7f75799b0500c633783d
Eliya Gerlitz <tent84e@gmail.com>
2023-04-21 00:13:46 +0300

    changed after NIV had explained

--- Frontend/src/Components/HomeArea/HomePage/HomePage.tsx
+++ Frontend/src/Components/HomeArea/HomePage/HomePage.tsx
@@ -3,7 +3,7 @@ import "./HomePage.css";
 function HomePage(): JSX.Element {
     return (
         <div className="HomePage">
-			I am HomePage
+			I am HomePage Modified after NIV had explained
             <img src="https://img.freepik.com/free-photo/full-shot-father-kid-with-tablet-floor_23-2149102996.jpg?size=626&ext=jpg&ga=GA1.2.1103567159.1597658053&semt=ais"/>
             <img src=""/>
         </div>
